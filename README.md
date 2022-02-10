55eefcb742e341ef0bd4578f082e7387665275d1
Denis Magda <dmagda@gridgain.com>
2022-02-10 15:27:37 -0500

    implemented the demo

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# -yugabyte-simple-csharp-app
-A simple C# app that connects to a Yugabyte cluster and performs basic CRUD operations.
+# Simple C# Application for YugabyteDB
+
+This application connects to your YugabyteDB instance via the 
+[Npgsql](https://docs.yugabyte.com/latest/reference/drivers/ysql-client-drivers/#npgsql) driver for PostgreSQL and performs basic SQL operations. The instructions below are provided for [Yugabyte Cloud](https://cloud.yugabyte.com/) deployments. 
+If you use a different type of deployment, then update the `sample-app.cs` file with proper connection parameters.
+
+## Prerequisite
+
+* [.NET 6.0 SDK](https://dotnet.microsoft.com/en-us/download) or later version.
+* Command line tool or your favourite IDE, such as Visual Studio Code.
+
+## Start Yugabyte Cloud Cluster
+
+* [Start YugabyteDB Cloud](https://docs.yugabyte.com/latest/yugabyte-cloud/cloud-quickstart/qs-add/) instance. You can use
+the free tier at no cost.
+* Add an IP address of your machine/laptop to the [IP allow list](https://docs.yugabyte.com/latest/yugabyte-cloud/cloud-secure-clusters/add-connections/#manage-ip-allow-lists)
+
+## Clone Application Repository
+
+Clone the repository and change dirs into it:
+
+```bash
+git clone https://github.com/yugabyte/yugabyte-simple-csharp-app && cd yugabyte-simple-csharp-app
+```
+
+## Provide Yugabyte Cloud Connection Parameters
+
+Locate and define the following connection parameters in the `sample-app.cs` file:
+* `urlBuilder.Host` - the hostname of your Yugabyte Cloud instance.
+* `urlBuilder.Username` - the username for your instance.
+* `urlBuilder.Password` - the database password.
+* `urlBuilder.SslCertificate` - a full path to your CA root cert (for example, `/Users/dmagda/certificates/root.crt`). 
+
+Note, you can easily find all the settings on the Yugabyte Cloud dashboard:
+
+![image](resources/cloud_app_settings.png)
+
+## Run the Application
+ 
+Build and run the application:
+```bash
+dotnet run
+```
+
+Upon successful execution, you will see output similar to the following:
+
+```bash
+>>>> Connecting to YugabyteDB!
+>>>> Successfully connected to YugabyteDB!
+>>>> Successfully created table DemoAccount.
+>>>> Selecting accounts:
+name = Jessica, age = 28, country = USA, balance = 10000
+name = John, age = 28, country = Canada, balance = 9000
+>>>> Transferred 800 between accounts
+>>>> Selecting accounts:
+name = Jessica, age = 28, country = USA, balance = 9200
+name = John, age = 28, country = Canada, balance = 9800
+```
+
+## Explore Application Logic
+
+Congrats! You've successfully executed a simple C# app that works with Yugabyte Cloud.
+
+Now, explore the source code of `sample-app.cs`:
+1. `connect` function - establishes a connection with your cloud instance via the libpq driver.
+3. `createDatabase` function - creates a table and populates it with sample data.
+4. `selectAccounts` function - queries the data with SQL `SELECT` statements.
+5. `transferMoneyBetweenAccounts` function - updates records consistently with distributed transactions.
+
+## Questions or Issues?
+
+Having issues running this application or want to learn more from Yugabyte experts?
+
+Join [our Slack channel](https://communityinviter.com/apps/yugabyte-db/register),
+or raise a question on StackOverflow and tag the question with `yugabytedb`!
---
+++ sample-app.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Npgsql;
+
+namespace Yugabyte_CSharp_Demo
+{
+    class SampleApp
+    {
+        static NpgsqlConnection connect() 
+        {
+            Console.WriteLine(">>>> Connecting to YugabyteDB!");
+
+            NpgsqlConnectionStringBuilder urlBuilder = new NpgsqlConnectionStringBuilder();
+            urlBuilder.Host = "";
+            urlBuilder.Port = 5433;
+            urlBuilder.Database = "yugabyte";
+            urlBuilder.Username = "";
+            urlBuilder.Password = "";
+            urlBuilder.SslMode = SslMode.Require;
+            urlBuilder.TrustServerCertificate = true;
+
+            // Doesn't work in this mode:
+            // urlBuilder.SslMode = SslMode.VerifyFull;
+            // urlBuilder.SslCertificate = "/Users/dmagda/Downloads/yb_cloud/root.pem";
+
+            NpgsqlConnection conn = new NpgsqlConnection(urlBuilder.ConnectionString);
+
+            conn.Open();
+
+            Console.WriteLine(">>>> Successfully connected to YugabyteDB!");
+
+            return conn;
+        }
+
+        static void createDatabase(NpgsqlConnection conn) 
+        {
+            NpgsqlCommand query = new NpgsqlCommand("DROP TABLE IF EXISTS DemoAccount", conn);
+            query.ExecuteNonQuery();
+
+            query = new NpgsqlCommand("CREATE TABLE DemoAccount (" +
+                        "id int PRIMARY KEY," +
+                        "name varchar," +
+                        "age int," +
+                        "country varchar," +
+                        "balance int)", conn);
+            query.ExecuteNonQuery();
+
+            query = new NpgsqlCommand("INSERT INTO DemoAccount VALUES" +
+                        "(1, 'Jessica', 28, 'USA', 10000)," +
+                        "(2, 'John', 28, 'Canada', 9000)", conn);
+            query.ExecuteNonQuery();
+
+            Console.WriteLine(">>>> Successfully created table DemoAccount.");
+        }
+
+        static void selectAccounts(NpgsqlConnection conn) 
+        {
+            Console.WriteLine(">>>> Selecting accounts:");
+
+            NpgsqlCommand query = new NpgsqlCommand("SELECT name, age, country, balance FROM DemoAccount", conn);
+
+            NpgsqlDataReader reader = query.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("name = {0}, age = {1}, country = {2}, balance = {3}",
+                    reader.GetString(0), reader.GetInt32(1), reader.GetString(2), reader.GetInt32(3));
+            }
+
+            reader.Close();
+        }
+
+        static void transferMoneyBetweenAccounts(NpgsqlConnection conn, int amount)
+        {
+            try
+            {
+            NpgsqlTransaction tx = conn.BeginTransaction();
+
+            NpgsqlCommand query = new NpgsqlCommand("UPDATE DemoAccount SET balance = balance - " + 
+                amount + " WHERE name = \'Jessica\'", conn, tx);
+            query.ExecuteNonQuery();
+
+            query = new NpgsqlCommand("UPDATE DemoAccount SET balance = balance + " + 
+                amount + " WHERE name = \'John\'", conn, tx);
+            query.ExecuteNonQuery();
+
+            tx.Commit();
+
+            Console.WriteLine(">>>> Transferred " + amount + " between accounts");
+
+            } catch (NpgsqlException ex) 
+            {
+                if (ex.SqlState != null && ex.SqlState.Equals("40001")) 
+                {
+                    Console.WriteLine("The operation is aborted due to a concurrent transaction that is modifying the same set of rows." +
+                            "Consider adding retry logic for production-grade applications.");
+                }
+
+                throw ex;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            NpgsqlConnection conn = null;
+            
+            try
+            {
+                conn = connect();
+
+                createDatabase(conn);
+                selectAccounts(conn);
+                transferMoneyBetweenAccounts(conn, 800);
+                selectAccounts(conn);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failure: " + ex.Message);
+            }
+            finally
+            {
+                if (conn != null && conn.State != System.Data.ConnectionState.Closed)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
+
